6feab00084ea207c63f3a8e541a180aa6a0e9829
balasundarsubramanian <balasundarsubramanian@gmail.com>
2024-06-01 17:31:35 +0530

    my branch code updated

--- src/test/java/testPackage/TestFitpeo.java
+++ src/test/java/testPackage/TestFitpeo.java
@@ -4,6 +4,15 @@ public class TestFitpeo {
 
 	
 	public static void main(String[] args) {
-		System.out.println("123");
+			System.out.println("Testing");
+			String s = "Object Oriented Programming Strcture";
+			String[] st = s.split(" ");
+			String n="";
+			for(int i=0;i< st.length -1;i++) {
+				char c = st[i].charAt(0); 
+				n=n+c;
+			}
+			System.out.println("Coding completed   :  " + n);
+	 System.out.println("Testing my branch code");
 	}
 }
